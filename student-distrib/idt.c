b1765f5383fa6e3c8160b9ebaddfb12d60b0c8d6
Gunaratne <dinalg2@illinois.edu>
2022-10-14 00:13:22 -0500

    exceptions written

---
+++ student-distrib/idt.c
@@ -0,0 +1,101 @@
+// #include "x86_desc.h"
+// #include "lib.h"
+
+// void div_by_zero_exception(){
+//     clear();
+//     printf("divide by zero exception raised");
+//     while(1);
+// }
+// void debug_exception(){
+//     clear();
+//     printf("debug exception raised");
+//     while(1);
+// }
+// void NMI_exception(){
+//     clear();
+//     printf("non-maskable interrupt exception raised");
+//     while(1);
+// }
+// void breakpoint_exception(){
+//     clear();
+//     printf("breakpoint exception raised");
+//     while(1);
+// }
+// void overflow_exception(){
+//     clear();
+//     printf("overflow exception raised");
+//     while(1);
+// }
+// void bound_range_exceeded_exception(){
+//     clear();
+//     printf("bound range exceeded exception raised");
+//     while(1);
+// }
+// void invalid_opcode_exception(){
+//     clear();
+//     printf("invalid opcode exception raised");
+//     while(1);
+// }
+// void device_not_available_exception(){
+//     clear();
+//     printf("device not available exception raised");
+//     while(1);
+// }
+// void double_fault_exception(){
+//     clear();
+//     printf("double fault exception raised");
+//     while(1);
+// }
+// void coprocessor_segment_overrun(){
+//     clear();
+//     printf("coprocessor segment overrun exception raised");
+//     while(1);
+// }
+// void invalid_TSS_exception(){
+//     clear();
+//     printf("invalid TSS exception raised");
+//     while(1);
+// }
+// void segment_not_present_exception(){
+//     clear();
+//     printf("segment not present exception raised");
+//     while(1);
+// }
+// void stack_fault_exception(){
+//     clear();
+//     printf("stack fault exception raised");
+//     while(1);
+// }
+// void general_protection_exception(){
+//     clear();
+//     printf("general protection exception raised");
+//     while(1);
+// }
+// void page_fault_exception(){
+//     clear();
+//     printf("page-fault exception raised");
+//     while(1);
+// }
+// void x87_FPU_fp_error(){
+//     clear();
+//     printf("x87 FPU floating-point exception raised");
+//     while(1);
+// }
+// void alignment_check_exception(){
+//     clear();
+//     printf("alignment check exception raised");
+//     while(1);
+// }
+// void machine_check_exception(){
+//     clear();
+//     printf("machine check exception raised");
+//     while(1);
+// }
+// void SIMD_fp_exception(){
+//     clear();
+//     printf("SIMD Floating-Point exception raised");
+//     while(1);
+// }
+
+
+
