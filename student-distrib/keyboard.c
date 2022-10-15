7540b92bfd438110f72b761ad603c8cff081f8be
Gupta <aryang4@illinois.edu>
2022-10-14 23:04:24 -0500

    :(

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,18 +1,30 @@
 #include "keyboard.h"
 #include "lib.h"
+#include "i8259.h"
+
+#define KEYBOARD_IRQ_NUM 1
+#define KEYBOARD_PORT 0x60
 
 // 0x60 is scan code
 
 char key_map[128] = {
-    0,'','1','2','3','4','5','6','7','8','9','0','-','=',
+    '\0',' ','1','2','3','4','5','6','7','8','9','0','-','=',
     '\b','\t','q','w','e','r','t','y','u','o','p','[',
-    ']','','','a','s','d','f','g','h','j','k','l',
-    '','','z','x','c','v','b','n','m','','','',
-}
+    ']','\n',' ','a','s','d','f','g','h','j','k','l',
+    ' ',' ','z','x','c','v','b','n','m',' ',' ',' ',
+};
 
 void keyboard_init(void){
-    
+    cli();
+    i8259_init();
+    enable_irq(KEYBOARD_IRQ_NUM);
+
 }
 
 
-void keyboard_handler();
+void keyboard_handler(){
+    unsigned char scan_code;
+    scan_code = inb(KEYBOARD_PORT);
+    putc(key_map[scan_code]);
+    send_eoi(KEYBOARD_IRQ_NUM);
+}
