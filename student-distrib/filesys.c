7c6cc42df8cd1c5e17ef91a6df09bcd164d5ce4c
Baala <kbaala2@illinois.edu>
2022-10-24 16:28:23 -0500

    comments

--- student-distrib/filesys.c
+++ student-distrib/filesys.c
@@ -9,22 +9,23 @@ uint32_t inode_num;
 
 
 int32_t file_init(boot_block_t *boot){
-    origin = boot;
-    root_inode = (origin + 1);
-    inode_num = origin->inode_count;
-    dentry_obj = &(origin->direntries[2]);
-    first_data_block = (data_block_t*)(origin + inode_num + 1);//* 4096; // inode_num * sizeof(inode_t)
+    origin = boot;                                              // ptr to boot block
+    root_inode = (origin + 1);                                  // ptr to first inode
+    inode_num = origin->inode_count;                            // holds total number of inodes 
+    dentry_obj = &(origin->direntries[2]);                      // ptr to 2nd dentry    
+    first_data_block = (data_block_t*)(origin + inode_num + 1); //contains ptr to the first data block
     return 0;
 }
 
 
 int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry){
     int i = 0;
-    if(index > ENTRY_NUM || index < 0) return -1;
+    if(index > ENTRY_NUM || index < 0) return -1;                                           //check if idx is within bounds
     
-    memcpy(dentry->filename, origin->direntries[index].filename, sizeof(dentry->filename));
-    dentry->filetype = origin->direntries[index].filetype;
-    dentry->inode_num = origin->direntries[index].inode_num;
+    /*copy to dentry object*/
+    memcpy(dentry->filename, origin->direntries[index].filename, sizeof(dentry->filename)); //copies filename to dentry object
+    dentry->filetype = origin->direntries[index].filetype;                                  //copies filetype to dentry object
+    dentry->inode_num = origin->direntries[index].inode_num;                                //copies inode number to dentry object
     return 0;
 }
 
@@ -32,11 +33,13 @@ int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry){
     int idx = 0; //file index
     int i;
 
-    if(strlen((int8_t*)fname) > FILENAME_LEN || fname == NULL) return -1;
+    if(strlen((int8_t*)fname) > FILENAME_LEN || fname == NULL) return -1; //check if the filename is not NULL and is at max 32 characters
 
+    /* go through each dentry to check if the filename exists in the dentry, then copy to dentry object and return*/
     for(idx = 0; idx < ENTRY_NUM; idx++){
-        if(strncmp((int8_t*)origin->direntries[idx].filename,(int8_t*)fname, sizeof(fname)) == 0){
+        if(strncmp((int8_t*)origin->direntries[idx].filename,(int8_t*)fname, sizeof(fname)) == 0){ 
             
+            /*copy to dentry object*/
             memcpy(dentry->filename, origin->direntries[idx].filename, sizeof(dentry->filename));
             dentry->filetype = origin->direntries[idx].filetype;
             dentry->inode_num = origin->direntries[idx].inode_num;
@@ -44,7 +47,7 @@ int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry){
         }
     }
     
-    return -1;
+    return -1; //return -1 when file name does not exist
 }
 
 int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length){
@@ -55,7 +58,7 @@ int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t lengt
     int i = 0;
     int end;
     uint32_t data_block_idx = offset/4096;
-    if(inode >= inode_num || data_block_idx >= DATA_NUMBER) return -1;
+    if(inode >= inode_num || data_block_idx >= DATA_NUMBER) return -1; //bounds checking
 
     inode_ptr = root_inode + inode; // get inode from inode offset
     datab_num = inode_ptr->data_block_num[data_block_idx];  //get data block num from inode specified by offset
@@ -77,7 +80,7 @@ int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t lengt
 
 int32_t open_f(const uint8_t* filename){
     if(filename == NULL) return -1;
-    return read_dentry_by_name(filename, dentry_obj);
+    return read_dentry_by_name(filename, dentry_obj); //open file by reading the filename
 }
 
 int write_f(int32_t fd, const void *buf, int32_t nbytes){
@@ -85,22 +88,21 @@ int write_f(int32_t fd, const void *buf, int32_t nbytes){
 }
 
 int close_f(int32_t fd){
-    
     return 0;
 }
 
 int read_f(int32_t fd, void *buf, int32_t nbytes){
-   
-    return read_data(fd,0, buf, nbytes);
+    return read_data(fd,0, buf, nbytes); //read file data in read file function
 }
 
 int32_t open_d(const uint8_t* filename){
     if(filename == NULL) return 0;
     read_dentry_by_name(filename, dentry_obj);
+    /*check if the stored dentry information is a directory type*/
     if(dentry_obj->filetype ==1){
         return 0;
     }
-    return -1;
+    return -1; //return -1 if file is not a directory
 }
 
 int write_d(int32_t fd, const void *buf, int32_t nbytes){
@@ -112,17 +114,18 @@ int close_d(int32_t fd){
 }
 
 int read_d(int32_t fd, void *buf, int32_t nbytes){
-    if(buf == NULL) return -1;
+    if(buf == NULL) return -1;         // null check
     if(fd > 62 || fd < 0 ) return -1;  //check  if file index out of bound
     int i =0;
 
+    /*loop to check how many bytes is contained in the filename*/
     for(i =0; i < 32; i++){
         if(origin->direntries[fd].filename[i] == NULL){
             nbytes = i;
             break;
         }
     }
-    memcpy(buf, origin->direntries[fd].filename, sizeof(origin->direntries[fd].filename));
+    memcpy(buf, origin->direntries[fd].filename, sizeof(origin->direntries[fd].filename)); //copy filename to buffer
     return nbytes;
 }
 
